ff87fd8a449f56ed92a699cdf93200d3beb800f8
isaacvrb <93296766+isaacvrb@users.noreply.github.com>
2021-11-15 10:10:24 -0300

    Criando arquivo JavaScript, e habilitando funcionalidade do menu

---
+++ assets/js/javascript.js
@@ -0,0 +1,7 @@
+function menuTogger(){
+    if (document.querySelector('nav').style.display == 'flex'){
+        document.querySelector('nav').style.display = 'none';
+    } else {
+        document.querySelector('nav').style.display = 'flex';
+    }
+}
