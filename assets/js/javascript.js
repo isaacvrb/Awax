cfe73b8f86a463e0e896edbf9ce13a7b3703c090
isaacvrb <93296766+isaacvrb@users.noreply.github.com>
2021-11-18 19:50:21 -0300

    Adcionei timer nos Sliders

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,27 +5,35 @@ function menuTogger(){
         document.querySelector('nav').style.display = 'flex';
     }
 }
-let slidersBanner = document.querySelector('#slidersBanner')
-let sp1 = document.querySelector('#sp1')
-let sp2 = document.querySelector('#sp2')
-let sp3 = document.querySelector('#sp3')
+let slidersBanner = document.querySelector('#slidersBanner');
+let sp1 = document.querySelector('#sp1');
+let sp2 = document.querySelector('#sp2');
+let sp3 = document.querySelector('#sp3');
+let timer;
 
 function primeiroSlide(){
+    clearInterval(timer)
     slidersBanner.style.marginLeft = "0";
-    sp2.classList.remove('active')
-    sp3.classList.remove('active')
-    sp1.classList.add('active')
+    sp2.classList.remove('active');
+    sp3.classList.remove('active');
+    sp1.classList.add('active');
+    timer = setInterval(segundoSlide , 5000);
+
 
 }
 function segundoSlide(){
+    clearInterval(timer)
     slidersBanner.style.marginLeft = "-100vw";
-    sp1.classList.remove('active')
-    sp3.classList.remove('active')
-    sp2.classList.add('active')
+    sp1.classList.remove('active');
+    sp3.classList.remove('active');
+    sp2.classList.add('active');
+    timer = setInterval(terceiroSlide , 5000);
 }
 function terceiroSlide(){
+    clearInterval(timer)
     slidersBanner.style.marginLeft = "-200vw";
-    sp1.classList.remove('active')
-    sp2.classList.remove('active')
-    sp3.classList.add('active')
+    sp1.classList.remove('active');
+    sp2.classList.remove('active');
+    sp3.classList.add('active');
+    timer = setInterval(primeiroSlide , 5000);
 }
