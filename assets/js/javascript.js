5dc7b3546e04303275f9a62080d76b0df39c6def
isaacvrb <93296766+isaacvrb@users.noreply.github.com>
2021-11-17 22:01:58 -0300

    Adcionando funcionalidade de JavaScrit nos Sliders

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,3 +5,27 @@ function menuTogger(){
         document.querySelector('nav').style.display = 'flex';
     }
 }
+let slidersBanner = document.querySelector('#slidersBanner')
+let sp1 = document.querySelector('#sp1')
+let sp2 = document.querySelector('#sp2')
+let sp3 = document.querySelector('#sp3')
+
+function primeiroSlide(){
+    slidersBanner.style.marginLeft = "0";
+    sp2.classList.remove('active')
+    sp3.classList.remove('active')
+    sp1.classList.add('active')
+
+}
+function segundoSlide(){
+    slidersBanner.style.marginLeft = "-100vw";
+    sp1.classList.remove('active')
+    sp3.classList.remove('active')
+    sp2.classList.add('active')
+}
+function terceiroSlide(){
+    slidersBanner.style.marginLeft = "-200vw";
+    sp1.classList.remove('active')
+    sp2.classList.remove('active')
+    sp3.classList.add('active')
+}
