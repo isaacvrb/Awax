4beeacb29d3430ec352cd27fdfef346255064632
isaacvrb <isaacbh0297@gmail.com>
2024-06-24 19:59:48 -0300

    Formatando arquivos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Projeto Awax é um projeto de Html e Css totalmente responsivo.
+
 Vizualize a pagina em https://isaacvrb.github.io/Awax/
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,106 +1,103 @@
-function menuTogger(){
-    if (document.querySelector('nav').style.display == 'flex'){
-        document.querySelector('nav').style.display = 'none';
-    } else {
-        document.querySelector('nav').style.display = 'flex';
-    }
+function menuTogger() {
+  if (document.querySelector('nav').style.display == 'flex') {
+    document.querySelector('nav').style.display = 'none';
+  } else {
+    document.querySelector('nav').style.display = 'flex';
+  }
 }
-const c = (el)=>document.querySelector(el);
+const c = el => document.querySelector(el);
 let timer;
 
-const sps = [c('#sp1'),c('#sp2'),c('#sp3')];
+const sps = [c('#sp1'), c('#sp2'), c('#sp3')];
 
-function mudarSlide(n){
-    clearInterval(timer)
-    c('#slidersBanner').style.marginLeft =  `${100 -(n * 100)}vw`;
+function mudarSlide(n) {
+  clearInterval(timer);
+  c('#slidersBanner').style.marginLeft = `${100 - n * 100}vw`;
 
-    const atual = sps[n-1];
+  const atual = sps[n - 1];
 
-    sps.forEach((sp) => {
-        if (sp !== atual) {
-            sp.classList.remove('active')
-        } else if (!atual.classList.contains('active')) {
-            atual.classList.add('active'); 
-        }
-    })
+  sps.forEach(sp => {
+    if (sp !== atual) {
+      sp.classList.remove('active');
+    } else if (!atual.classList.contains('active')) {
+      atual.classList.add('active');
+    }
+  });
 
-    timer = setInterval(() => {
-        if (sps.length === n ){
-            mudarSlide(1);
-        } else {
-            mudarSlide(n+1)
-        }
-    }, 5000);
+  timer = setInterval(() => {
+    if (sps.length === n) {
+      mudarSlide(1);
+    } else {
+      mudarSlide(n + 1);
+    }
+  }, 5000);
 }
 
-c('#otb1').addEventListener('click', ()=>{
+c('#otb1').addEventListener('click', () => {
+  c('#otb2').classList.remove('active');
+  c('#otb3').classList.remove('active');
+  c('#otb1').classList.add('active');
 
-    c('#otb2').classList.remove('active');
-    c('#otb3').classList.remove('active');
-    c('#otb1').classList.add('active');
+  c('#ots1').setAttribute('src', 'media/mulher1.png');
+  c('#ots2').setAttribute('src', 'media/homem1.png');
+  c('#ots3').setAttribute('src', 'media/mulher2.png');
 
-    c('#ots1').setAttribute('src', 'media/mulher1.png');
-    c('#ots2').setAttribute('src', 'media/homem1.png');
-    c('#ots3').setAttribute('src', 'media/mulher2.png');
-
-    c('#stn1').innerHTML = 'Jane Doe';
-    c('#stn2').innerHTML = 'Mike Chiller';
-    c('#stn3').innerHTML = 'Anna Jhons';
-
+  c('#stn1').innerHTML = 'Jane Doe';
+  c('#stn2').innerHTML = 'Mike Chiller';
+  c('#stn3').innerHTML = 'Anna Jhons';
 });
 
-c('#otb2').addEventListener('click', ()=>{
-    c('#otb1').classList.remove('active');
-    c('#otb3').classList.remove('active');
-    c('#otb2').classList.add('active');
+c('#otb2').addEventListener('click', () => {
+  c('#otb1').classList.remove('active');
+  c('#otb3').classList.remove('active');
+  c('#otb2').classList.add('active');
 
-    c('#ots1').setAttribute('src', 'media/homem1.png');
-    c('#ots2').setAttribute('src', 'media/homem2.png');
-    c('#ots3').setAttribute('src', 'media/mulher1.png');
+  c('#ots1').setAttribute('src', 'media/homem1.png');
+  c('#ots2').setAttribute('src', 'media/homem2.png');
+  c('#ots3').setAttribute('src', 'media/mulher1.png');
 
-    c('#stn1').innerHTML = 'Mike Chiller';
-    c('#stn2').innerHTML = 'Jhon Doe';
-    c('#stn3').innerHTML = 'Jane Doe';
+  c('#stn1').innerHTML = 'Mike Chiller';
+  c('#stn2').innerHTML = 'Jhon Doe';
+  c('#stn3').innerHTML = 'Jane Doe';
 });
 
-c('#otb3').addEventListener('click', ()=>{
-    c('#otb1').classList.remove('active');
-    c('#otb2').classList.remove('active');
-    c('#otb3').classList.add('active');
+c('#otb3').addEventListener('click', () => {
+  c('#otb1').classList.remove('active');
+  c('#otb2').classList.remove('active');
+  c('#otb3').classList.add('active');
 
-    c('#ots1').setAttribute('src', 'media/homem2.png');
-    c('#ots2').setAttribute('src', 'media/mulher2.png');
-    c('#ots3').setAttribute('src', 'media/homem1.png');
+  c('#ots1').setAttribute('src', 'media/homem2.png');
+  c('#ots2').setAttribute('src', 'media/mulher2.png');
+  c('#ots3').setAttribute('src', 'media/homem1.png');
 
-    c('#stn1').innerHTML = 'Jhon Doe';
-    c('#stn2').innerHTML = 'Anna Jhons';
-    c('#stn3').innerHTML = 'Mike Chiller';
+  c('#stn1').innerHTML = 'Jhon Doe';
+  c('#stn2').innerHTML = 'Anna Jhons';
+  c('#stn3').innerHTML = 'Mike Chiller';
 });
 
-const hcs = [c('#hcb1'), c('#hcb2'), c('#hcb3')]
+const hcs = [c('#hcb1'), c('#hcb2'), c('#hcb3')];
 let timer2;
 
-function happySliders(n){
-    clearInterval(timer2)
+function happySliders(n) {
+  clearInterval(timer2);
 
-    c('#slidersBannerClients').style.marginLeft = `${100 - (n * 100)}vw`;
+  c('#slidersBannerClients').style.marginLeft = `${100 - n * 100}vw`;
 
-    const hcAtual = hcs[n-1];
+  const hcAtual = hcs[n - 1];
 
-    hcs.forEach((hc)=>{
-        if (hc !== hcAtual) {
-            hc.classList.remove('active')
-        } else if (!hcAtual.classList.contains('active')) {
-            hcAtual.classList.add('active');
-        }
-    });
-    
-    timer2 = setInterval(() => {
-        if (hcs.length === n ){
-            happySliders(1);
-        } else {
-            happySliders(n+1)
-        }
-    } , 7000);
+  hcs.forEach(hc => {
+    if (hc !== hcAtual) {
+      hc.classList.remove('active');
+    } else if (!hcAtual.classList.contains('active')) {
+      hcAtual.classList.add('active');
+    }
+  });
 
-};
+  timer2 = setInterval(() => {
+    if (hcs.length === n) {
+      happySliders(1);
+    } else {
+      happySliders(n + 1);
+    }
+  }, 7000);
+}
